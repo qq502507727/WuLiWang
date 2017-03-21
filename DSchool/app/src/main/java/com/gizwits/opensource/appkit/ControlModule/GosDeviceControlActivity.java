9fbab7154e7f4d50c3300a7b8378e2e00496dd81
闫伟男 <502507727@qq.com>
2017-03-21 15:10:36 +0800

    机智云项目

---
+++ DSchool/app/src/main/java/com/gizwits/opensource/appkit/ControlModule/GosDeviceControlActivity.java
@@ -0,0 +1,61 @@
+package com.gizwits.opensource.appkit.ControlModule;
+
+import com.gizwits.gizwifisdk.api.GizWifiDevice;
+import com.gizwits.opensource.appkit.R;
+import com.gizwits.opensource.appkit.CommonModule.GosBaseActivity;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class GosDeviceControlActivity extends GosBaseActivity {
+
+	/** The tv MAC */
+	private TextView tvMAC;
+
+	/** The GizWifiDevice device */
+	private GizWifiDevice device;
+
+	/** The ActionBar actionBar */
+	ActionBar actionBar;
+//	private static final String KEY_RED_SWITCH = "LED_onOff";
+//	private static final String KEY_LIGHT_COLOR ="LED_Color";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_gos_device_control);
+		initDevice();
+		setActionBar(true, true, device.getProductName());
+		initView();
+	}
+
+	private void initView() {
+		tvMAC = (TextView) findViewById(R.id.tvMAC);
+		if (null != device) {
+
+			tvMAC.setText(device.getMacAddress().toString());
+
+		}
+	}
+
+	private void initDevice() {
+		Intent intent = getIntent();
+		device = (GizWifiDevice) intent.getParcelableExtra("GizWifiDevice");
+		Log.i("Apptest", device.getDid());
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			this.finish();
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+}
